fc85f5ef09849f3116ffb151e55a4a497869b533
SUBHAM Acharya <42038161+subham1399@users.noreply.github.com>
2020-01-11 01:48:26 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,39 @@ import './App.css';
 import {Button , Form , FormGroup , Label , Input} from 'reactstrap';
 
 class App extends Component {
+  state={
+    Agenda:"",
+    Type:"",
+    Total_Speaker_Time:"" ,
+    Description:"",
+    raised_by:"" 
+
+  };
+ 
+  handleChangeAgenda = event => {
+   // console.log(event.target.value);
+    this.setState({ Agenda: event.target.value});
+    /*this.setState({ Type: event.target.value});
+    this.setState({ Total_Speaker_Time: event.target.value});
+    this.setState({ Description: event.target.value});
+    this.setState({ raised_by: event.target.value});*/
+  };
+
+  handleChangeType = event => {
+    this.setState({ Type: event.target.value});
+  };
+
+  handleChangeTST = event => {
+    this.setState({ Total_Speaker_Time: event.target.value});
+  };
+  handleChangeDescription = event =>{
+    this.setState({ Description: event.target.value});
+  };
+  handleChangeraisedby = event =>{
+    this.setState({ raised_by: event.target.value});
+  };
+
+
   render() {
     
     return (
@@ -70,13 +103,13 @@ class App extends Component {
 
                 <div class="input">
                   
-                <input type="text"  name="FirstName" class="no-outline"  />
+                <input type="text" value={this.state.name} onChange={this.handleChangeAgenda} name="FirstName" class="no-outline"  />
                 </div>
                 <br/>
                 <label style={{marginRight:"400px"}}><b>Type</b></label><br/>
                 <div class="input">
                 
-                <input type="text"  name="FirstName" class="no-outline"  />
+                <input type="text" value={this.state.Type} onChange={this.handleChangeType}  name="FirstName" class="no-outline"  />
                 </div>
                 <br/>
                 <br/>
@@ -84,7 +117,7 @@ class App extends Component {
                 <label style={{marginRight:"285px"}}><b>Total Speaker Time</b></label><br/></div>
                 <div class="input">
                 
-                <input type="text"  name="FirstName" class="no-outline"  />
+                <input type="text" value={this.state.Total_Speaker_Time} onChange={this.handleChangeTST}  name="FirstName" class="no-outline"  />
                 
                 </div>
                 <br/>
@@ -92,7 +125,7 @@ class App extends Component {
                 <label style={{marginRight:"350px"}}><b>Description</b></label><br/>
                 <div class="input">
                 
-                <input type="text"  name="FirstName" class="no-outline"  />
+                <input type="text" value={this.state.Description} onChange={this.handleChangeDescription} name="FirstName" class="no-outline"  />
                 </div>
                 <br/>
                 <br/>
@@ -100,7 +133,7 @@ class App extends Component {
                 <div class="input">
                
                
-                <input type="text"  name="FirstName" class="no-outline"  />
+                <input type="text" value={this.state.raised_by} onChange={this.handleChangeraisedby} name="FirstName" class="no-outline"  />
               
                 </div>
               <br/>
